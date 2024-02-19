8fbebd159d42637e9f888e0f929c7decddcbff28
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-19 18:06:19 +0100

    add: store and config primo compontente test...init project  complite

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import{ store } from './store';
+import AppTest from './components/AppTest.vue';
+
+export default {
+  data() {
+    
+    return {
+      store,
+     
+    }
+  },
+  components:{AppTest},
+};
 </script>
 
 <template>
- 
-  <HelloWorld/>
+  <div class="container">
+    <h1>New project</h1>
+    <h3>{{ store.globalVarTest }}</h3>
+    <AppTest/>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/AppTest.vue
@@ -0,0 +1,23 @@
+<script>
+import { store } from "../store";
+
+export default{
+  data(){
+    return{
+     prova:'prova'
+
+    }
+  },
+};
+
+
+
+</script>
+
+<template>
+   <h3>{{ prova }}</h3> 
+  <h4>componente  AppTest collegato</h4>
+</template>
+
+<style scoped>
+</style>
