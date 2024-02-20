9b97bf1a902e50caa6f7f6a9dc02b1916d4957f7
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-20 15:16:31 +0100

    add subscribbe section

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import AppTest from './components/AppTest.vue';
 import AppHeader from './components/AppHeader.vue'
 import AppFooter from './components/AppFooter.vue';
 import AppHero from './components/AppHero.vue';
+import AppSubscribe from './components/AppSubscribe.vue'
 
 export default {
   data() {
@@ -13,20 +14,20 @@ export default {
      
     }
   },
-  components:{AppTest,AppHeader,AppFooter,AppHero},
+  components:{AppTest,AppHeader,AppFooter,AppHero,AppSubscribe},
 };
 </script>
 
 <template>
   <AppHeader/>
-
   <AppHero/> 
-  <div class="container">
-   
-  </div>
+  <AppSubscribe/>
   <AppFooter/>
 </template>
 
 <style lang="scss" scoped>
+//import of var and mixins
+@use './styles/partials/variables.scss' as *;
+@use './styles/partials/mixins.scss' as *;
 
 </style>
---
+++ src/components/AppSubscribe.vue
@@ -0,0 +1,74 @@
+<script>
+import { store } from "../store";
+
+export default{
+  data(){
+    return{
+      store,
+      prova:'prova',
+     
+    }
+  },
+};
+
+
+
+</script>
+
+<template>
+    <section class="subscribe-section">
+
+        <div class="subscribe text-center">
+            <div class="d-flex gap-3 justify-content-center">
+                <h3>Subscribe</h3>
+                <h4>Newsletters</h4>
+            </div>
+            <div class="mb-5">Enter your email address to register to our newsletter subscription 
+                delivered on a regular basis!
+            </div>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Enter your email...">
+                <button class="btn-subscribe btn btn-outline-secondary" type="button">Subscribe</button>
+            </div>
+        </div>
+        
+    </section>
+</template>
+
+<style lang="scss" scoped>
+//import of var and mixins
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
+
+.subscribe-section{
+    height: 420px;
+    background-color: $white-color;
+    @include center-flex;
+    .subscribe{
+        border: 2px solid red;
+        width: 660px;
+        padding: 25px 50px;
+        h3{
+            font-size: 48px;
+            font-weight: 600;
+        }
+        h4{
+            font-size: 48px;
+            font-weight: 600;
+            color: $gren-color;
+        }
+        .btn{
+            color: white;
+            background-color: $gren-color;
+            border: none;
+            font-size: 14px;
+            font-weight: 800;
+            padding: 15px 30px;
+        }
+    }
+    
+
+}
+</style>
+
+
